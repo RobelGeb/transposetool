17b0e8325598a3010995dd2b1f4351a0a64ab504
RobelGeb <52620546+RobelGeb@users.noreply.github.com>
2020-08-26 18:01:22 -0700

    Update Transpose.java
    Added some documentation/commenting, prepping for future JFugue integration

--- Transpose.java
+++ Transpose.java
@@ -1,21 +1,34 @@
+//Robel Gebremichael
+//Transposition tool; small personal project
+
 import java.awt.*;
 import java.util.*;
 import java.io.*;
+//import org.jfugue.player.Player;
+
 
 public class Transpose {
 
    public static final int CON = 25; 
 
    public static void main(String[] args) throws FileNotFoundException{
-   
-      Scanner input = new Scanner(new File("og_notes.txt"));
-      ArrayList<String> newNotes = new ArrayList<String>();
       
-      newNotes = txtReader(input, newNotes);
+      //scans in the source file for the notes, their octave values, and time signature
+      Scanner inputNotes = new Scanner(new File("og_notes_num.txt"));
+      Scanner inputOct = new Scanner(new File("og_notes_num.txt"));
+      ArrayList<String> newNotes = new ArrayList<String>();
+      ArrayList<String> timeSig = new ArrayList<String>();
+      ArrayList<Integer> octaves = new ArrayList<Integer>();
+
+      //rips notes, time signature, and octave values from .txt file, respectively
+      newNotes = noteReader(inputNotes, newNotes);
+      timeSig = timeReader(inputNotes, timeSig);
+      octaves = octReader(inputOct, octaves);
       
       String[] notes = new String[] {"Bb", "B", "C", "C#", "D", "Eb", "E", "F", "F#", "G", "Ab", "A"};
       String[] notesForDraw = new String[] {"B", "C", "D", "E", "F", "G", "A"}; 
-   
+      
+      //stores all instruments in a map based on key, ranging from C, Eb, F, and Bb
       HashMap<String, String> instrumentMap = 
          new HashMap<String, String>() {
             {
@@ -47,14 +60,66 @@ public class Transpose {
       System.out.println(Arrays.toString(newNotes.toArray()));
       
       
+      /*
+      FOR DRAWING "SHEET MUSIC"
+      
       DrawingPanel panel = new DrawingPanel(40 * CON, 10 * CON);
 
       Graphics g = panel.getGraphics();
-      Font font = new Font("Times New Roman", Font.PLAIN, 24);
+      Font font = new Font("Times New Roman", Font.PLAIN, 25);
       g.setFont(font);
       
       System.out.println("Now drawing sheet music...");
-      drawIt(g, newNotes, notesForDraw);
+      drawIt(g, newNotes, notesForDraw, timeSig, octaves);
+      */
+   }
+   
+   //pre: takes in scanner object and arraylist that stores the notes
+   //post:takes every note in the inputted text document and puts it in an Array List
+   public static ArrayList<String> noteReader(Scanner inputNotes, ArrayList<String> newNotes) {
+      if (inputNotes.hasNextLine()) {
+         String line = inputNotes.nextLine();
+         Scanner newLine = new Scanner(line);
+         int tokenNum = 0;
+         while (newLine.hasNext()) {
+            if (tokenNum % 2 == 0) {
+               newNotes.add(newLine.next());
+            } else {
+               String toss = newLine.next();
+            }
+            tokenNum++;
+         }
+      }
+      return newNotes;
+   }
+   
+   //gets time signature from txt doc
+   public static ArrayList<String> timeReader(Scanner inputNotes, ArrayList<String> timeSig) {
+      if (inputNotes.hasNextLine()) {
+         String line = inputNotes.nextLine();
+         Scanner newLine = new Scanner(line);
+         while (newLine.hasNext()) {
+            timeSig.add(newLine.next());
+         }
+      }
+      return timeSig;
+   }
+   
+   public static ArrayList<Integer> octReader(Scanner inputOct, ArrayList<Integer> octaves) {
+      if (inputOct.hasNextLine()) {
+         String line = inputOct.nextLine();
+         Scanner newLine = new Scanner(line);
+         int tokenNum = 0;
+         while (newLine.hasNext()) {
+            if (tokenNum % 2 == 1) {
+               octaves.add(newLine.nextInt());
+            }  else {
+               String toss = newLine.next();
+            }
+            tokenNum++;
+         }
+      }
+      return octaves; 
    }
    
    //takes console input for what instrument you want to transpose your music from
@@ -122,24 +187,20 @@ public class Transpose {
       steps = (secondPos - firstPos) * -1;
       return steps;      
    }
-
-   //takes every note in the inputted text document and puts it in an Array List
-
-   public static ArrayList<String> txtReader(Scanner input, ArrayList<String> newNotes) {
-      while (input.hasNext()) {
-         newNotes.add(input.next());
-   }
-      return newNotes;
-   }
    
-   public static ArrayList<String> halfStepper(Scanner console, int halfSteps, String[] notes, ArrayList<String> newNotes) {
+   //CHANGE ALGORITHM SO THAT THE NOTE LOOP ONLY TAKES EVERY OTHER TOKEN STARTING AT THE FIRST ONE (TO ONLY TAKE THE LETTERS)
+   //AND THE OCTAVE TOKEN LOOP STARTS AT THE SECOND TOKEN AND TAKES EVERY OTHER TOKEN ALSO, SO THE HALFSTEP ALGORITHM DOESN'T GET 
+   //CONFUSED WITH THE NUMBERS.
+   
+   public static ArrayList<String> halfStepper(Scanner console, int halfSteps, String[] notes,
+                                              ArrayList<String> newNotes) {
       //first loop handles going to each note in the array
       for (int i = 0; i < newNotes.size(); i++) {
          //second loop handles changing each note 
          for (int j = 0; j <= notes.length; j++) {
             if (newNotes.get(i).equals(notes[j])) {
                if ((j + halfSteps) > notes.length - 1) { 
-                  newNotes.set(i, notes[(j + halfSteps) - notes.length]); 
+                  newNotes.set(i, notes[(j + halfSteps) - notes.length]);
                } else if ((j + halfSteps) < 0) {
                   newNotes.set(i, notes[(j + halfSteps) + notes.length]);
                } else {
@@ -148,31 +209,41 @@ public class Transpose {
                break;
             } 
          }
-      }
+      } 
       return newNotes;
    }
    
-   public static void drawIt(Graphics g, ArrayList<String> newNotes, String[] notesForDraw) {
+   //post: draws makeshift "sheet music" without the note length values, only their tones, using the drawing panel
+   //relatively low res
+   public static void drawIt(Graphics g, ArrayList<String> newNotes, String[] notesForDraw, ArrayList<String> timeSig,
+                              ArrayList<Integer> octaves) {
+      
       int subtractor = 0;
+      
       for (int i = 1; i <= 5; i++) {
-         g.drawLine(CON, CON + (CON * i), 39 * CON, CON + (CON * i));
+         g.drawLine(0, CON + (CON * i), 39 * CON, CON + (CON * i));
       }
       g.drawLine(CON, CON * 2, CON, CON * 6);
       g.drawLine(CON * 39, CON * 2, CON * 39, CON * 6);
       for (int i = 0; i < newNotes.size(); i++) {
-         for (int j = 0; j < notesForDraw.length; j++) {
+         
+         int j = 0;
+         String note = newNotes.get(i);
+         int oct = octaves.get(i);
+         
+         for (j = 0; j < notesForDraw.length; j++) {
             if (newNotes.get(i).contains(notesForDraw[j])) {
                subtractor = j;
                if (j < 2) {
-                  g.drawLine(CON * 2 + (i * 2 * CON), CON + (CON * 7) - 15, CON * 2 + 30 + (i * 2 * CON), CON + (CON * 7) - 15);
+                  g.drawLine(CON * 2 + (i * 2 * CON), CON + (CON * 6), CON * 2 + 20 + (i * 2 * CON), CON + (CON * 6));
                }
+               g.drawString(newNotes.get(i), CON * 2 + (i * 2 * CON), (CON / 2) * (16 - subtractor) + (4 - oct) * 75);
                break;
             }
          }
-         g.drawString(newNotes.get(i), CON * 2 + (i * 2 * CON), ((CON / 2) * (16 - subtractor)));
       }
       
+      g.drawString(timeSig.get(0), 5, CON * 4);
+      g.drawString(timeSig.get(1), 5, CON * 5);
    }
 }
-   
-// add numbers to the original notes scheme to identify octaves 
